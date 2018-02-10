6d959e4ce477e03612769f456a9be40cb490794f
Liana Pigeot <liana.pigeot@gmail.com>
2018-02-10 23:03:13 +0000

    First commit using stdweb fork to access the WebGL context

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rust-webGL-engine
-Trying to build a proof of concept Rust WebGL engine
+Trying to build a proof of concept basic Rust 3D WebGL engine
+
+Currently using my fork of [stdweb](https://github.com/nialna/stdweb/tree/webglRenderer) with a WebGLRenderingContext, because stdweb doesn't have it yet.
+
+Will eventually use the main plugin when it gets there.
+
+Only fills the screen using `gl.clear();` for now
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate stdweb;
+
+use stdweb::unstable::TryInto;
+use stdweb::web::{
+	IParentNode,
+	IHtmlElement,
+    document,
+    WebGLRenderingContext,
+
+};
+
+
+use stdweb::web::html_element::CanvasElement;
+
+fn main() {
+	stdweb::initialize();
+
+	let canvas: CanvasElement = document().query_selector( "#canvas" ).unwrap().unwrap().try_into().unwrap();
+	let context: WebGLRenderingContext = canvas.get_context().unwrap();
+
+	canvas.set_width(canvas.offset_width() as u32);
+	canvas.set_height(canvas.offset_height() as u32);
+
+	context.clear_color(1.0, 0.0, 0.0, 1.0);
+	context.clear();
+
+	stdweb::event_loop();
+}
